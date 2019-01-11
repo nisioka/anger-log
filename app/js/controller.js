065f8b8dbc2514717c77a5ee8e234063041aae7b
daisuke.nishioka <nishioka38@gmail.com>
2019-01-11 13:01:04 +0900

    写経(2)。 https://qiita.com/daiki7nohe/items/769c8f2f60a0e94c5e56#実際に使ってみる２

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,8 +1,9 @@
 var app = angular.module('App', []);
 
 app.controller('AppController', function($scope){
-    $scope.display = function(){
-        var str = $scope.displayText;
-        $scope.text = str;
-    }
+    $scope.comments = [
+        {name: "Yamada", text: "Hello!"},
+        {name: "Tanaka", text: "Thanks!"},
+        {name: "Nakamura", text: "Yeah!"}
+    ];
 });
