78119e665b8a09817ceaa2d84112d371ec858d96
daisuke.nishioka <nishioka38@gmail.com>
2019-01-30 14:14:30 +0900

    アンガーログ項目を列挙。

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,12 +1,22 @@
 var app = angular.module('App', []);
 
 app.controller('AppController', function($scope){
-    $scope.username = '';
+    $scope.date = '';
+    $scope.time = '';
+    $scope.title = '';
+    $scope.case = '';
+    $scope.think = '';
+    $scope.emotion = '';
+    $scope.strength = '';
+    $scope.action = '';
+    $scope.result = '';
+
     $scope.users = [];
     $scope.submit = function () {
         $scope.users.push({
-            username: $scope.username,
-            url: '//twitter.com/' + $scope.username
+            date: $scope.date,
+            time: $scope.time,
+            title: $scope.title
         });
     };
 });
