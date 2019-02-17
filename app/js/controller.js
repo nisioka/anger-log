6a5d2ad887408b77bdd667e179990e66428c68e8
nisioka <nishioka.daisuke@gmail.com>
2019-02-17 20:03:00 +0900

    toastを試してみる。 http://tech-blog.tsukaby.com/archives/709

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,14 +1,7 @@
-var app = angular.module('App', ['ngStorage']);
+var app = angular.module('App', ['ngStorage', 'ngAnimate', 'toaster']);
 
-app.controller('AppController', ['$scope', '$localStorage', function ($scope, $localStorage) {
-    $scope.dateTime = '';
-    $scope.title = '';
-    $scope.case = '';
-    $scope.think = '';
-    $scope.emotion = '';
-    $scope.strength = '';
-    $scope.action = '';
-    $scope.result = '';
+app.controller('AppController', ['$scope', '$localStorage', function ($scope, $localStorage, toaster, $window) {
+    clear($scope);
 
     $scope.$storage = $localStorage.$default({
         logs: []
@@ -27,5 +20,36 @@ app.controller('AppController', ['$scope', '$localStorage', function ($scope, $l
             result: $scope.result
         });
         $scope.$storage.logs = $scope.logs;
+        clear($scope);
+    };
+
+    $scope.pop = function(){
+        toaster.pop('success', "title", 'Its address is https://google.com.', 15000, 'trustedHtml', 'goToLink');
+        toaster.pop('success', "title", '<ul><li>Render html</li></ul>', 5000, 'trustedHtml');
+        toaster.pop('error', "title", '<ul><li>Render html</li></ul>', null, 'trustedHtml');
+        toaster.pop('wait', "title", null, null, 'template');
+        toaster.pop('warning', "title", "myTemplate.html", null, 'template');
+        toaster.pop('note', "title", "text");
+    };
+
+    $scope.goToLink = function(toaster) {
+        var match = toaster.body.match(/http[s]?:\/\/[^\s]+/);
+        if (match) $window.open(match[0]);
+        return true;
+    };
+
+    $scope.clear = function(){
+        toaster.clear();
     };
 }]);
+
+function clear($scope) {
+    $scope.dateTime = '';
+    $scope.title = '';
+    $scope.case = '';
+    $scope.think = '';
+    $scope.emotion = '';
+    $scope.strength = '';
+    $scope.action = '';
+    $scope.result = '';
+}
